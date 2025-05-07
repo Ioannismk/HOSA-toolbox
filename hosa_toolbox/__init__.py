adc0c4dc83cc238a3b57cc8b3eaf1bdf860b1dfe
Ioannismk <150161327+Ioannismk@users.noreply.github.com>
2025-05-07 19:14:45 +0300

    Update __init__.py

--- hosa_toolbox/__init__.py
+++ hosa_toolbox/__init__.py
@@ -10,9 +10,20 @@ Created on Wed May  7 15:56:20 2025
 from .cumulants import cum2est, cum2x, cum3est, cum4est
 from .bispectrum import bispecd, plot_bispecd
 from .bicoherence import bicoher, plot_bicoher
+from .bispectral_features import (
+    bpsec_areas,
+    bspec_total_area,
+    bspec_max_area,
+    bspec_entropy,
+    bspec_sq_entropy,
+    dominant_peak,
+    find_dom_peak_radius,
+    compute_bispectral_features
+)
 
 __all__ = [
     "cum2est", "cum2x", "cum3est", "cum4est",
     "bispecd", "plot_bispecd",
-    "bicoher", "plot_bicoher"
+    "bicoher", "plot_bicoher", "bspec_areas","bspec_total_area", "bspec_max_area",
+    "bspec_entropy","bspec_sq_entropy","dominant_peak", "find_dom_peak_radius", "compute_bispectral_features"
 ]

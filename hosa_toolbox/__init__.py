eaf18bd12fda47de2f7c05703d154a0faf7614f5
Ioannismk <150161327+Ioannismk@users.noreply.github.com>
2025-05-07 16:52:25 +0300

    Update __init__.py

--- hosa_toolbox/__init__.py
+++ hosa_toolbox/__init__.py
@@ -8,11 +8,11 @@ Created on Wed May  7 15:56:20 2025
 # hosa_toolbox/__init__.py
 
 from .cumulants import cum2est, cum2x, cum3est, cum4est
-from .bispectrum import bispecd, bispectrum_direct, plot_bispecd
+from .bispectrum import bispecd, plot_bispecd
 from .bicoherence import bicoher, plot_bicoher
 
 __all__ = [
     "cum2est", "cum2x", "cum3est", "cum4est",
-    "bispecd", "bispectrum_direct", "plot_bispecd",
+    "bispecd", "plot_bispecd",
     "bicoher", "plot_bicoher"
 ]
